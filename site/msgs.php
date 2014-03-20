43103c28201e6bdd1a67a7721bb48f956c9d09e8
kimory <kimory@nuguet.fr>
2014-03-20 11:10:17 +0100

    réinitialisation de la partie site

--- site/msgs.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('_JEXEC') or die;
-
-$task = JFactory::getApplication()->input->get('task');
-
-$controller	= JControllerLegacy::getInstance('Msgs');
-$controller->execute(JFactory::getApplication()->input->get('task'));
-$controller->redirect();
