f3485ccd735aa454d717856979b6135871104f0a
kimory <kimory@nuguet.fr>
2014-03-23 16:39:34 +0100

    ajout de la gestion des droits

--- admin/msgs.php
+++ admin/msgs.php
@@ -1,6 +1,12 @@
 <?php
 defined('_JEXEC') or die;
 
+// Vérifie l'accès au composant
+if (!JFactory::getUser()->authorise('core.manage', 'com_msgs'))
+{
+	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+}
+
 $task = JFactory::getApplication()->input->get('task');
 
 $controller	= JControllerLegacy::getInstance('Msgs');
